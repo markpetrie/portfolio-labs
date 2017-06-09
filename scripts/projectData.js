c193a6afd364e76484d01a1d06bda0dc60ef4f84
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-06-08 17:33:26 -0700

    hey mark

---
+++ scripts/projectData.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var projectData = [{
+    title: 'Project One',
+    tags: ['PM', ' Dev'],
+    roles: ['PM', ' Dev'],
+    description: 'This is a project description',
+    image: 'this will be an image path'
+},
+{
+    title: 'Project One',
+    tags: ['PM', ' Dev'],
+    roles: ['PM', ' Dev'],
+    description: 'This is a project description',
+    image: 'this will be an image path'
+},
+{
+    title: 'Project One',
+    tags: ['PM', ' Dev'],
+    roles: ['PM', ' Dev'],
+    description: 'This is a project description',
+    image: 'this will be an image path'
+},
+];
