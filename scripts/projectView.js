d31fc33808e6040b9c40245567d6475a61d50560
Mark Petrie <petrie.mark@gmail.com>
2017-06-29 15:00:03 -1000

    Store project data in a .json file, use localStorage to persist project data, use jQuery and AJAX to asynchronously retrieve data.

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,17 +1,19 @@
 'use strict';
 
-var projectView = {};
+const projectView = {};
 
 projectView.handleMainNav = function () {
     $('.tab').on('click', function () {
         $('.tab-content').hide();
-
         var section = '#' + $(this).attr('data-content');
         $(section).show();
     });
     $('.main-nav .tab:first').click();
 };
 
-$(document).ready(function () {
-    projectView.handleMainNav();
-});
+projectView.initIndexPage = function() {
+  Project.all.forEach(function(project) {
+    $('#projects').append(project.toHtml())
+  });
+  projectView.handleMainNav();
+};
